6a01818aa79ca4f09c95412a9de513d2255d91bd
Victor Torres <vatorresm88@gmail.com>
2023-12-11 15:23:45 -0500

    AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+
+import { JwtService } from '@nestjs/jwt';
+import * as bcryptjs from 'bcryptjs';
+import { UsersService } from 'src/users/users.service';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async register({ password, email, name }: RegisterDto) {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (user) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    await this.usersService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return {
+      message: 'User created successfully',
+    };
+  }
+
+  async login({ email, password }: LoginDto) {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid email');
+    }
+
+    const isPasswordValid = await bcryptjs.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    return {
+      email: user.email,
+    };
+  }
+}
