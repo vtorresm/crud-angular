58cfa834f965b6571bf005a0c06a1b27b3e5fca5
Victor Torres <vatorresm88@gmail.com>
2023-12-13 15:41:44 -0500

    Creando constante jwt.constant

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,7 @@
 import {
-  BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
-import { RegisterDto } from './dto/register.dto';
 
 import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from 'bcryptjs';
@@ -12,27 +10,10 @@ import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
-
-  async register({ password, email, name }: RegisterDto) {
-    const user = await this.usersService.findOneByEmail(email);
-
-    if (user) {
-      throw new BadRequestException('Email already exists');
-    }
-
-    const hashedPassword = await bcryptjs.hash(password, 10);
-
-    await this.usersService.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    return {
-      message: 'User created successfully',
-    };
-  }
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async login({ email, password }: LoginDto) {
     const user = await this.usersService.findOneByEmail(email);
@@ -47,7 +28,12 @@ export class AuthService {
       throw new UnauthorizedException('Invalid password');
     }
 
+    const payload = { email: user.email };
+
+    const token = await this.jwtService.signAsync(payload);
+
     return {
+      token: token,
       email: user.email,
     };
   }
