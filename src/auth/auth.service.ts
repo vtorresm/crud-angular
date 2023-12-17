6ed98c31a23ef7e7ce318909033a59e7cfcb93b3
Victor Torres <vatorresm88@gmail.com>
2023-12-17 16:10:21 -0500

    Agregando funci+on register

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -7,6 +8,7 @@ import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from 'bcryptjs';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,6 +16,26 @@ export class AuthService {
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
   ) {}
+
+  async register({ password, email, name }: RegisterDto) {
+    const user = await this.usersService.findOneByEmail(email);
+
+    if (user) {
+      throw new BadRequestException('Email already exists');
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    await this.usersService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return {
+      message: 'User created successfully',
+    };
+  }
 
   async login({ email, password }: LoginDto) {
     const user = await this.usersService.findOneByEmail(email);
