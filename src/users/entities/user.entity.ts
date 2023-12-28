b3e58fdf7e80a9d0fa0ea7f0cece01d969f0a00f
Victor Torres <vatorresm88@gmail.com>
2023-12-28 18:47:18 -0500

    Agregando Global Hidden password

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,7 +11,7 @@ export class User {
   @Column({ unique: true, nullable: false })
   email: string;
 
-  @Column({ nullable: false })
+  @Column({ nullable: false, select: false })
   password: string;
 
   @Column({ default: 'user' })
